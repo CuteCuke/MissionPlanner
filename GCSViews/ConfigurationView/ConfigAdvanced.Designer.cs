435c50d78f3da6d644359d66b17d6aa97424a7eb
LK <625018452@qq.com>
2020-04-27 15:40:07 +0800

    4.27  15.40

--- GCSViews/ConfigurationView/ConfigAdvanced.Designer.cs
+++ GCSViews/ConfigurationView/ConfigAdvanced.Designer.cs
@@ -37,20 +37,20 @@
             this.BUT_outputMavlink = new MissionPlanner.Controls.MyButton();
             this.BUT_outputnmea = new MissionPlanner.Controls.MyButton();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
             this.BUT_follow_me = new MissionPlanner.Controls.MyButton();
             this.label8 = new System.Windows.Forms.Label();
-            this.BUT_paramgen = new MissionPlanner.Controls.MyButton();
             this.label9 = new System.Windows.Forms.Label();
-            this.BUT_movingbase = new MissionPlanner.Controls.MyButton();
+            this.BUT_paramgen = new MissionPlanner.Controls.MyButton();
             this.label10 = new System.Windows.Forms.Label();
+            this.BUT_movingbase = new MissionPlanner.Controls.MyButton();
             this.but_anonlog = new MissionPlanner.Controls.MyButton();
-            this.label11 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -114,9 +114,7 @@
             this.tableLayoutPanel1.Controls.Add(this.BUT_outputMavlink, 0, 4);
             this.tableLayoutPanel1.Controls.Add(this.but_proximity, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.but_signkey, 0, 3);
-            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.label3, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.label11, 1, 9);
             this.tableLayoutPanel1.Controls.Add(this.BUT_follow_me, 0, 6);
             this.tableLayoutPanel1.Controls.Add(this.label8, 1, 6);
             this.tableLayoutPanel1.Controls.Add(this.label9, 1, 7);
@@ -124,7 +122,29 @@
             this.tableLayoutPanel1.Controls.Add(this.label10, 1, 8);
             this.tableLayoutPanel1.Controls.Add(this.BUT_movingbase, 0, 8);
             this.tableLayoutPanel1.Controls.Add(this.but_anonlog, 0, 9);
+            this.tableLayoutPanel1.Controls.Add(this.label11, 1, 9);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
             // 
             // label2
             // 
@@ -136,25 +156,10 @@
             resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
             // 
-            // label4
+            // label11
             // 
-            resources.ApplyResources(this.label4, "label4");
-            this.label4.Name = "label4";
-            // 
-            // label5
-            // 
-            resources.ApplyResources(this.label5, "label5");
-            this.label5.Name = "label5";
-            // 
-            // label6
-            // 
-            resources.ApplyResources(this.label6, "label6");
-            this.label6.Name = "label6";
-            // 
-            // label7
-            // 
-            resources.ApplyResources(this.label7, "label7");
-            this.label7.Name = "label7";
+            resources.ApplyResources(this.label11, "label11");
+            this.label11.Name = "label11";
             // 
             // BUT_follow_me
             // 
@@ -168,6 +173,11 @@
             resources.ApplyResources(this.label8, "label8");
             this.label8.Name = "label8";
             // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
             // BUT_paramgen
             // 
             resources.ApplyResources(this.BUT_paramgen, "BUT_paramgen");
@@ -175,10 +185,10 @@
             this.BUT_paramgen.UseVisualStyleBackColor = true;
             this.BUT_paramgen.Click += new System.EventHandler(this.BUT_paramgen_Click);
             // 
-            // label9
+            // label10
             // 
-            resources.ApplyResources(this.label9, "label9");
-            this.label9.Name = "label9";
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
             // 
             // BUT_movingbase
             // 
@@ -187,22 +197,12 @@
             this.BUT_movingbase.UseVisualStyleBackColor = true;
             this.BUT_movingbase.Click += new System.EventHandler(this.BUT_movingbase_Click);
             // 
-            // label10
-            // 
-            resources.ApplyResources(this.label10, "label10");
-            this.label10.Name = "label10";
-            // 
             // but_anonlog
             // 
             resources.ApplyResources(this.but_anonlog, "but_anonlog");
             this.but_anonlog.Name = "but_anonlog";
             this.but_anonlog.UseVisualStyleBackColor = true;
             this.but_anonlog.Click += new System.EventHandler(this.but_anonlog_Click);
-            // 
-            // label11
-            // 
-            resources.ApplyResources(this.label11, "label11");
-            this.label11.Name = "label11";
             // 
             // ConfigAdvanced
             // 
