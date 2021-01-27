9bfb177a309c2cd0e03bd7a96b97cde671483897
lk <625018452@qq.com>
2021-01-27 14:05:46 +0800

    close photo bug

--- MainV2.cs
+++ MainV2.cs
@@ -4643,10 +4643,10 @@ namespace MissionPlanner
                     lastwpstr = MainV2.comPort.MAV.cs.lastautowp.ToString();
                     return_now_lat = MainV2.comPort.MAV.cs.lat;
                     return_now_lng = MainV2.comPort.MAV.cs.lng;
-                    //if (MainV2.comPort.doCommand((byte)MainV2.comPort.sysidcurrent, (byte)MainV2.comPort.compidcurrent, MAVLink.MAV_CMD.DO_SET_CAM_TRIGG_DIST, 0, 0, 1, 0, 0, 0, 0))
-                    //{ }
-                    //else
-                    //    CustomMessageBox.Show(Strings.CommandFailed+"关闭拍照失败", Strings.ERROR); 
+                    if (MainV2.comPort.doCommand((byte)MainV2.comPort.sysidcurrent, (byte)MainV2.comPort.compidcurrent, MAVLink.MAV_CMD.DO_SET_CAM_TRIGG_DIST, 0, 0, 1, 0, 0, 0, 0))
+                    { }
+                    else
+                        CustomMessageBox.Show(Strings.CommandFailed + "关闭拍照失败", Strings.ERROR);
 
                     if (wpno >= 3)
                     {
@@ -4668,15 +4668,15 @@ namespace MissionPlanner
                         else if (MainV2.comPort.MAV.cs.firmware == Firmwares.ArduPlane)
                         {
                             //MainV2.comPort.doCommand(MAVLink.MAV_CMD.DO_SET_CAM_TRIGG_DIST, 0, 0, 1, 0, 0, 0, 0);
-                            for (int i = wpno - 5; i >= wpno / 2; i--)
-                            {
-                                if (comPort.getWP((ushort)i).id == (ushort)MAVLink.MAV_CMD.DO_SET_CAM_TRIGG_DIST)
-                                {
-                                    MainV2.comPort.setWPCurrent(MainV2.comPort.MAV.sysid, MainV2.comPort.MAV.compid,
-                                       (ushort)(i));
-                                    break;
-                                }
-                            }
+                            //for (int i = wpno - 5; i >= wpno / 2; i--)
+                            //{
+                            //    if (comPort.getWP((ushort)i).id == (ushort)MAVLink.MAV_CMD.DO_SET_CAM_TRIGG_DIST)
+                            //    {
+                            //        MainV2.comPort.setWPCurrent(MainV2.comPort.MAV.sysid, MainV2.comPort.MAV.compid,
+                            //           (ushort)(i));
+                            //        break;
+                            //    }
+                            //}
                             MainV2.comPort.setWPCurrent(MainV2.comPort.MAV.sysid, MainV2.comPort.MAV.compid,
                                         (ushort)(wpno - 4));
                         }
